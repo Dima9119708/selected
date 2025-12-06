4bae9547506e7ef2fda5f892b69e493d37af6636
dima9119708 <tipanswer@gmail.com>
2025-12-06 13:12:48 +0200

    feature/1
    feature/1

--- src/app/app.vue
+++ src/app/app.vue
@@ -39,9 +39,9 @@ console.log('9933333333333333333333999999999999999333333333333333333339999999999
 
 console.log('==== STAGE ====')
 console.log('==== STAGE ====')
-console.log('==== STAGE ====')
-console.log('==== STAGE ====')
-console.log('==== STAGE ====')
+console.log('==== MAIN ====')
+console.log('==== MAIN ====')
+console.log('==== MAIN ====')
 console.log('==== STAGE ====')
 const router = useRouter()
 const i18n = useI18n({
